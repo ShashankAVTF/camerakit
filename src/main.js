3ad92a9b3640b1bfd0fcdbf5cf421aa32ee755a6
Shashank <Shashank@avtftechnology.com>
2024-11-28 16:48:41 +0530

    commit2

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
 import{
-    bootstrapCameraKit,
-    createMediaStreamSource,
-    Transform2D,
+  bootstrapCameraKit,
+  createMediaStreamSource,
+  Transform2D,
 }from '@snap/camera-kit'
 
 (async function () {
-    
-    var cameraKit= await bootstrapCameraKit({
-        apiToken:'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3OTIzLCJzdWIiOiI5YzRkNjMxNy1iYmI5LTRjODQtOWYzMC0wZTVmMzA3NDUxZGZ-U1RBR0lOR34zYjY4MTBkYy0xN2Y2LTRmNzctYTNiZS02YTdjMjU0Zjc2YjgifQ.La2HprjDjaVTHUAEgc7hvsk2xKx7krdqfvuPwdXzN9o'
-    });
+  
+  var cameraKit= await bootstrapCameraKit({
+      apiToken:'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3OTIzLCJzdWIiOiI5YzRkNjMxNy1iYmI5LTRjODQtOWYzMC0wZTVmMzA3NDUxZGZ-U1RBR0lOR34zYjY4MTBkYy0xN2Y2LTRmNzctYTNiZS02YTdjMjU0Zjc2YjgifQ.La2HprjDjaVTHUAEgc7hvsk2xKx7krdqfvuPwdXzN9o'
+  });
 
-    const session= await cameraKit.createSession()
-    document.getElementById('canvas').replaceWith(session.output.live)
+  const session= await cameraKit.createSession()
+  document.getElementById('canvas').replaceWith(session.output.live)
 
-    const {lenses} = await cameraKit.lensRepository.loadlensGroups(['67e7b4ff-7878-46fd-b778-adbf25e9a722'])
+  const {lenses} = await cameraKit.lensRepository.loadlensGroups(['67e7b4ff-7878-46fd-b778-adbf25e9a722'])
 
-    session.applyLens(lenses[0])
+  session.applyLens(lenses[0])
 
-    let mediaStream = await navigator.mediaDevices.getUserMedia({video:true});
-    
-    const source= createMediaStreamSource(mediaStream,{
-        transform: Transform2D.MirrorX,
-        cameraType: 'front'
-    })
+  let mediaStream = await navigator.mediaDevices.getUserMedia({video:true});
+  
+  const source= createMediaStreamSource(mediaStream,{
+      transform: Transform2D.MirrorX,
+      cameraType: 'front'
+  })
 
-    await session.setSource(source);
-    session.source.setRenderSize(window.innerWidth,window.innerHeight)
+  await session.setSource(source);
+  session.source.setRenderSize(window.innerWidth,window.innerHeight)
 
-    session.play();
+  session.play();
 
 })();
